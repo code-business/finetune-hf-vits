1fdb576a504b7af8da7ac72b753b2463ce9410b7
Dhaval Gala <dhaval.gala59@gmail.com>
2024-01-30 11:59:35 +0530

    new dataset

--- tsv_to_parquet.py
+++ tsv_to_parquet.py
@@ -3,9 +3,11 @@ import pyarrow as pa
 import pyarrow.parquet as pq
 from datasets import Dataset, Audio
 
-base_path = '/home/dhaval/Downloads/cv-corpus-16.1-2023-12-06-mr/cv-corpus-16.1-2023-12-06'
+# base_path = '/home/dhaval/Downloads/cv-corpus-16.1-2023-12-06-mr/cv-corpus-16.1-2023-12-06'
+base_path = '/home/dhaval/Downloads/openslr-marathi-dataset'
 lang = 'mr'
-csv_file = f'{base_path}/{lang}/train.tsv'
+# csv_file = f'{base_path}/{lang}/train.tsv'
+csv_file = f'{base_path}/line_index.tsv'
 
 audio_paths = []
 text_arr = []
@@ -15,17 +17,18 @@ df = pd.read_csv(csv_file, sep='\t')
 df = df.reset_index()
 for index, row in df.iterrows():
     # if index >= 128 and index < 256:
-    audio_paths.append(f"{base_path}/{lang}/clips/{row['path']}")
-    text_arr.append(row["sentence"])
-    speaker_ids.append(row["client_id"])
+    # audio_paths.append(f"{base_path}/{lang}/clips/{row['path']}")
+    audio_paths.append(f"{base_path}/{row['Path']}.wav")
+    text_arr.append(row["Text"])
+    # speaker_ids.append(row["client_id"])
 
 audio_dataset = Dataset.from_dict({
     "audio": audio_paths,
     "text": text_arr,
-    "speaker_id": speaker_ids
+    # "speaker_id": speaker_ids
 }).cast_column("audio", Audio())
 
-audio_dataset.push_to_hub("dhavalgala/mozilla-mar")
+audio_dataset.push_to_hub("dhavalgala/openslr-mar")
 # print(audio_paths)
 
 # for i, chunk in enumerate(csv_stream):
